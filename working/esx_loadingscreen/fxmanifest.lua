fa27159aeea5ca09aa0d1e22890b4a511d1d9af3
klepchino-a11y <klepchino@gmail.com>
2025-10-28 09:04:58 +0000

    Add files via upload

--- working/esx_loadingscreen/fxmanifest.lua
+++ working/esx_loadingscreen/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'WestSide المنطقة الغربية'
-description 'WestSide (WS) themed loading screen inspired by Diriyah layout'
+description 'WestSide'
 version '3.0.0'
 
 loadscreen 'index.html'
@@ -11,6 +11,7 @@ loadscreen_manual_shutdown 'yes'
 
 shared_script 'config.lua'
 client_script 'client/client.lua'
+client_script 'client/force_close.lua'
 
 files {
   'index.html',
